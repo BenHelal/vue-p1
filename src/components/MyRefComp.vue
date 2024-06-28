a629d55cdf74c99030f9df64477acd959993d9cc
benhelal <benhelalwajdi@gmail.com>
2024-06-28 13:32:45 -0400

    done Ref Comp

--- src/components/MyRefComp.vue
+++ src/components/MyRefComp.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 let count = ref(0)
 let username = ref('')
 let userList = ref(['Wajdi', 'ben', 'helal'])
+let user = ref({name:'wajdi', age:20, location: ['nyc', 'idk'], isMarried: false}) 
 </script>
 
 <template>
@@ -16,4 +17,14 @@ let userList = ref(['Wajdi', 'ben', 'helal'])
 
   <h1>userList : {{ userList }}</h1>
   <button @click="userList.push('wbh')">add</button>
+
+  <h1>Name: {{ user.name }}</h1>
+  <h1>age: {{ user.age }}</h1>
+  <h1>location: {{ user.location }}</h1>
+  <h1>isMarried: {{ user.isMarried }}</h1>
+
+  <button @click="user.name = 'ben helal'">Change Name</button>
+  <button @click="user.age = 29">change Age</button>
+  <button @click="user.location = ['earth']">change Age</button>
+  <button @click="user.isMarried = true">change Age</button>
 </template>
